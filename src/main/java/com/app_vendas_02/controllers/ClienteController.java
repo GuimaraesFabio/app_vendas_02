111b74baefa003762484759bb113c454a05d2cc2
GuimaraesFabio <guimaraes.familia2018@gmail.com>
2020-03-19 15:00:47 -0300

    Implementado o metodo update (ClienteService, ClienteResource

--- src/main/java/com/app_vendas_02/controllers/ClienteController.java
+++ src/main/java/com/app_vendas_02/controllers/ClienteController.java
@@ -3,12 +3,15 @@ package com.app_vendas_02.controllers;
 import java.util.List;
 
 import com.app_vendas_02.domains.Cliente;
+import com.app_vendas_02.dtos.ClienteDto;
 import com.app_vendas_02.services.ClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +38,13 @@ public class ClienteController {
         Cliente obj = _service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody ClienteDto objDto, @PathVariable Integer id) {
+        Cliente obj = _service.fromDto(objDto);
+        obj.setId(id);
+        _service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/app_vendas_02/services/ClienteService.java
+++ src/main/java/com/app_vendas_02/services/ClienteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.app_vendas_02.domains.Cliente;
+import com.app_vendas_02.dtos.ClienteDto;
 import com.app_vendas_02.repositories.ClienteRepository;
 import com.app_vendas_02.services_exceptions.ObjectNotFoundException;
 
@@ -26,5 +27,20 @@ public class ClienteService {
     public Cliente findById(Integer id) {
         Optional<Cliente> obj = _repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return _repository.save(newObj);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public Cliente fromDto(ClienteDto objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
     }
 }
